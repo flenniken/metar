458c9758f0f382b6d99d37a098bce4338ded8cbf
Steve Flenniken <steve.flenniken@gmail.com>
2017-10-16 16:52:14 -0700

    Factor code

--- metar/readBytes.nim
+++ metar/readBytes.nim
@@ -1,6 +1,43 @@
 import endians
 
-## Read numbers from a file.
+## Read numbers from a file or byte buffer.
+
+proc length*[T](buffer: var openArray[uint8], index=0, endian: Endianness=littleEndian): T =
+  ## Return a number from the buffer at the given index with the
+  ## specified endianness.
+  ##
+  ## .. code-block:: nim
+  ##   import readBytes, os
+  ##   var buffer = [0x01'u8, 0x23, 0x45, 0x67, 0x89, 0xab, 0xcd, 0xef]
+  ##   var num16 = length[uint16](buffer)
+  ##   echo toHex(num16)
+  ##   2301
+  ##   num16 = length[uint16](buffer, 3, bigEndian)
+  ##   echo toHex(num16)
+  ##   6789
+
+  var pointer = addr(buffer[index])
+  when sizeof(T) == 1:
+    copyMem(addr(result), pointer, 1)
+    return
+  if endian == littleEndian:
+    when sizeof(T) == 2:
+      littleEndian16(addr(result), pointer)
+    elif sizeof(T) == 4:
+      littleEndian32(addr(result), pointer)
+    elif sizeof(T) == 8:
+      littleEndian64(addr(result), pointer)
+    else:
+      assert(false, "Invalid type")
+  else:
+    when sizeof(T) == 2:
+      bigEndian16(addr(result), pointer)
+    elif sizeof(T) == 4:
+      bigEndian32(addr(result), pointer)
+    elif sizeof(T) == 8:
+      bigEndian64(addr(result), pointer)
+    else:
+      assert(false, "Invalid type")
 
 proc read_number*[T](file: File, endian: Endianness=littleEndian): T =
   ## Read a number from the current file position.
@@ -18,54 +55,4 @@ proc read_number*[T](file: File, endian: Endianness=littleEndian): T =
   if file.readBytes(buffer, 0, sizeof(T)) != sizeof(T):
     raise newException(IOError, "Error reading file.")
 
-  if system.cpuEndian == endian:
-    result = cast[T](buffer)
-  else:
-    when sizeof(T) == 1:
-      result = cast[T](buffer)
-    elif sizeof(T) == 2:
-      swapEndian16(addr(result), addr buffer)
-    elif sizeof(T) == 4:
-      swapEndian32(addr(result), addr buffer)
-    elif sizeof(T) == 8:
-      swapEndian64(addr(result), addr buffer)
-    else:
-      assert(false, "Invalid type")
-
-proc length*[T](buffer: var openArray[uint8], index=0, endian: Endianness=littleEndian): T =
-  ## Return a number from the buffer at the given index with the
-  ## specified endianness.
-  ## .. code-block:: nim
-  ##   import readBytes, os
-  ##   var buffer = [0x01'u8, 0x23, 0x45, 0x67, 0x89, 0xab, 0xcd, 0xef] 
-  ##   var num16 = length[uint16](buffer)
-  ##   echo toHex(num16)
-  ##   2301
-  ##   num16 = length[uint16](buffer, 3, bigEndian)
-  ##   echo toHex(num16)
-  ##   6789
-
-  var pointer = addr(buffer[index])
-  if endian == littleEndian:
-    when sizeof(T) == 1:
-      copyMem(addr(result), pointer, 1)
-    elif sizeof(T) == 2:
-      littleEndian16(addr(result), pointer)
-    elif sizeof(T) == 4:
-      littleEndian32(addr(result), pointer)
-    elif sizeof(T) == 8:
-      littleEndian64(addr(result), pointer)
-    else:
-      assert(false, "Invalid type")
-  else:
-    when sizeof(T) == 1:
-      copyMem(addr(result), pointer, 1)
-    elif sizeof(T) == 2:
-      bigEndian16(addr(result), pointer)
-    elif sizeof(T) == 4:
-      bigEndian32(addr(result), pointer)
-    elif sizeof(T) == 8:
-      bigEndian64(addr(result), pointer)
-    else:
-      assert(false, "Invalid type")
-  
+  result = length[T](buffer, 0, endian)
